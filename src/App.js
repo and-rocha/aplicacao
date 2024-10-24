31c8faa0ab6a0bc1ec70e7d7db36f214721c45ae
Andre Rocha <androch1977@gmail.com>
2024-10-23 22:31:54 -0300

    Versao 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esse é meu Primeiro App.
+          Pipeline executado com sucesso!
         </p>
         <a
           className="App-link"
